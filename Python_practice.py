c2ff5664423afb9678be0b766bfe04005e551ae2
SATHISH1412 <46257322+SATHISH1412@users.noreply.github.com>
2023-01-22 14:17:51 +0000

    codespace check

--- Python_practice.py
+++ Python_practice.py
@@ -3,3 +3,6 @@ keys=it.values()
 print(keys)
 for iteam_name, iteam_value in it.items():
     print(f"This iteam I would like to take at {iteam_name} and this is {iteam_value}")
+
+wc="Hello Sathish"
+print(wc)
