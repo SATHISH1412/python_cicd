1dbf800a3240ce88bd62eac027f91f6de8c83c3f
Sathish <sathish121393@gmail.com>
2023-01-22 19:29:36 +0530

    Firstfile

---
+++ Python_practice.py
@@ -0,0 +1,5 @@
+it = {"Break": "Salad", "lunch": "Meals", "Dinner": "Sapathi"}
+keys=it.values()
+print(keys)
+for iteam_name, iteam_value in it.items():
+    print(f"This iteam I would like to take at {iteam_name} and this is {iteam_value}")
